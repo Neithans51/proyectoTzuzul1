c740404625676ce017649add11862487281a15d4
Neithans Valdez <SOPORTE@OTIC03.safonapp.gob.ve>
2022-09-20 14:37:11 -0400

    base de proyecto/2

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
-import {getYesNoInput} from "./utils/index.js";
+import { getInput } from "./utils/index.js";
 
-alert(getYesNoInput());
+const firstResponse = getInput([
+    'Agarrar espada',
+    'Correr',
+    'Dialogar'
+]);
 
+// switch(firstResponse){
+//     case "1": 
+//         getInput([
+//             'Ataque ligero',
+//             'Ataque pesado',
+//             'Escapar'
+//         ])
+//         break;
+//     case "2":
+//         alert('Te han matado mientras corrias, game over');
+//         break;
+//     case "3": 
+//         getInput([
+//             'Te robaron, persiguelos',
+//             'Te robaron, escapa de la ciudad'
+//         ]); 
+//     default: 
+//     break;
+// }
+
+if(firstResponse == "1"){
+    getInput([
+        'Ataque ligero',
+        'Ataque pesado',
+        'Escapar'
+    ])
+}else if(firstResponse == "2"){
+    alert('Te han matado mientras corrias, game over');
+}else{
+    getInput([
+        'Te robaron, persiguelos',
+        'Te robaron, escapa de la ciudad'
+    ]);
+}
